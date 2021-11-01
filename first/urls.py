67943bd966c6ba7589554989e03ec0198598ea43
emrhssla <emrhssla@gmail.com>
2021-11-01 13:27:52 +0900

    Url mapping rule

--- first/urls.py
+++ first/urls.py
@@ -6,4 +6,15 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('select/', views.select, name='select'),
     path('result/', views.result, name='result'),
+
+    # path parameter / form parameter / query parameter
+    # 변수화: 기본 path converter
+    #  path('articles/2003/', views.special_case_2003)
+    #  path('articles/<int:year>/<int:month>/', views.month_archive),
+    #  path('articles/<int:year>/<int:month>/<slug:slug>/', views.article_detail)
+
+
+    # 변수화2: 정규식 사용
+    # re_path(r'^articles/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.month_archive),
+    # re_path(r'^articles/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<slug>[\w-]+)/$', views.article_detail),
 ]
