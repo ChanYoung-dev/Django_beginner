4378b3a5159bee5baee1afed52fc7d468040066a
emrhssla <emrhssla@gmail.com>
2021-11-02 21:00:24 +0900

    템플릿 조건 처리
     템플릿 내에서 조건에 따라 다른 메세지를 띄우기

--- first/views.py
+++ first/views.py
@@ -4,22 +4,30 @@ from django.template import loader
 from datetime import datetime
 
 def index(request):
-    template = loader.get_template('index.html')
+    # django.shortcuts의 render 기능때문에
+    # template = loader.get_template('index.html')
+    # 위 구문을 지워도 된다.
     now = datetime.now()
     context = {
         'current_date': now
     }
-    return HttpResponse(template.render(context, request))
+    # django.shortcuts의 render 기능때문에
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
 
 
 def select(request):
-    message = '수 하나를 입력해주세요.'
-    return HttpResponse(message)
+    context = {
+        'number': 4
+    }
+    return render(request, 'select.html', context)
 
 
 def result(request):
-    message = '추첨 결과입니다.'
-    return HttpResponse(message)
+    context = {
+        'numbers': [1, 2, 3, 4]
+    }
+    return render(request, 'result.html', context)
 
 
 
