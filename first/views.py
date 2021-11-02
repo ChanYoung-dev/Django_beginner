44c03b9aa9f386e399718b3fcaa123f8451405d1
emrhssla <emrhssla@gmail.com>
2021-11-02 20:26:19 +0900

    템플릿 기본(변수 넘기기)

--- first/views.py
+++ first/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello world.")
+    template = loader.get_template('index.html')
+    now = datetime.now()
+    context = {
+        'current_date': now
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def select(request):
