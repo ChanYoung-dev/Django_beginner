07a8f18bb5d8d290c18b7ef078282c8a1445d28f
emrhssla <emrhssla@gmail.com>
2021-11-04 15:14:57 +0900

    블락 개념 적용시키기
    사이트의 반복되는 html 코드를 한 곳에 모아두고 재사용해서 사용할 수 있습니다. 이 때 사용되는게 django template의 block이란 개념

--- first/views.py
+++ first/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.template import loader
 from datetime import datetime
 
+import random
+
 def index(request):
     # django.shortcuts의 render 기능때문에
     # template = loader.get_template('index.html')
@@ -17,18 +19,40 @@ def index(request):
 
 
 def select(request):
-    context = {
-        'number': 4
-    }
+    context = {}
     return render(request, 'first/select.html', context)
+
+'''
+def result(request):
+    chosen = request.GET['number']  ## number라는 값으로 전달받은 데이터를 꺼내옵니다.
+    context = {'numbers': [chosen, 2, 3, 4, 5, 6]}  ## 첫 데이터를 받은 데이터로 넣습니다.
+    return render(request, 'first/result.html', context)
+'''
 
 
 def result(request):
-    context = {
-        'numbers': [1, 2, 3, 4]
-    }
-    return render(request, 'first/result.html', context)
+    chosen = int(request.GET['number'])
 
+    results = []
+    # 만약 수가 범위를 초과하지 않으면 결과 값에 미리 선택한 수를 넣는다.
+    if chosen >= 1 and chosen <= 45:
+        results.append(chosen)
+
+    # 값을 꺼낼 박스를 마련한다.
+    box = []
+    for i in range(0, 45):
+        if chosen != i+1:
+            box.append(i+1)
+
+    # 랜덤하게 뒤섞는다.
+    random.shuffle(box)
+
+    # results 개수가 6개가 될 때 까지 값을 하나씩 꺼낸다.
+    while len(results) < 6:
+        results.append(box.pop())
+
+    context = {'numbers':results}
+    return render(request, 'first/result.html', context)
 
 
 
