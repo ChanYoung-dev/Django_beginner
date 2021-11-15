fde7dde663cd84d47a49302f0dd166ff826e7b87
emrhssla <emrhssla@gmail.com>
2021-11-15 15:00:03 +0900

    폼으로 데이터 전송하기

--- second/views.py
+++ second/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from second.models import Post
 from .forms import PostForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def list(request):
@@ -12,3 +13,18 @@ def list(request):
 def create(request):
     form = PostForm()
     return render(request, 'second/create.html', {'form': form})
+
+'''
+def create(request):
+    context = {
+        'form': PostForm()
+    }
+    return render(request, 'second/create.html', context)
+'''
+
+def confirm(request):
+    form = PostForm(request.POST)  # request의 POST 데이터들을 바로 PostForm에 담을 수 있습니다.
+    if form.is_valid(): # 데이터가 form 클래스에서 정의한 조건 (max_length 등)을 만족하는지 체크합니다.
+        return render(request, 'second/confirm.html', {'form': form})
+    return HttpResponseRedirect('second/create/')  # 데이터가 유효하지 않으면 되돌아갑니다.
+
