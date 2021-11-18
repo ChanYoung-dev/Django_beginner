e40e3fd936b9bb489ef6178d0a406bb6d3670ed6
emrhssla <emrhssla@gmail.com>
2021-11-18 23:34:26 +0900

    CRUD -R 페이지구현하기

--- third/views.py
+++ third/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from third.models import Restaurant
+from django.core.paginator import Paginator
+
 
 def list(request):
+    restaurants = Restaurant.objects.all()
+    paginator = Paginator(restaurants, 5)  # 한 페이지에 5개씩 표시
+
+    page = request.GET.get('page')  # query params에서 page 데이터를 가져옴
+    items = paginator.get_page(page)  # 해당 페이지의 아이템으로 필터링
+
     context = {
-        'restaurants': Restaurant.objects.all()
+        'restaurants': items
     }
     return render(request, 'third/list.html', context)
+
