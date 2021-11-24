92e9a230d99623727c5b4b01712d67edebdb5f3b
emrhssla <emrhssla@gmail.com>
2021-11-24 17:40:11 +0900

    생성, 수정 Form에 비밀번호 입력 적용하기

--- third/models.py
+++ third/models.py
@@ -5,6 +5,9 @@ class Restaurant(models.Model): # Restaurant 라는 상점을 나타내는 모�
     name = models.CharField(max_length=30)  # 이름
     address = models.CharField(max_length=200)  # 주소
 
+    password = models.CharField(max_length=20, default=None, null=True)
+    image = models.CharField(max_length=500, default=None, null=True)
+
     created_at = models.DateTimeField(auto_now_add=True)  # 글 작성 시 (이 모델의 데이터(레코드) 저장 시) 생성 시각
     updated_at = models.DateTimeField(auto_now=True)  # 저장된 레코드 수정 시 수정 시각
 
